9b17a241644c07ea6ed7a3aa67fb8e896c6d41e5
u11932 <yussayev@alfabank.kz>
2022-10-13 11:41:42 +0600

    read file

--- fileapi/get.go
+++ fileapi/get.go
@@ -1,1 +1,12 @@
 package fileapi
+
+import "io/ioutil"
+
+func Read(path string) ([]byte, error) {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
+}
