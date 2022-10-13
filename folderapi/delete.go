012d9f3b030fbd40a49a3b8f8be7de2806187e16
u11932 <yussayev@alfabank.kz>
2022-10-13 11:45:53 +0600

    delete folder with content

--- folderapi/delete.go
+++ folderapi/delete.go
@@ -2,6 +2,12 @@ package folderapi
 
 import "os"
 
-func Delete(path string) error {
+func Delete(path string, withContent bool) error {
+	if withContent {
+		if err := os.RemoveAll(path); err != nil {
+			return err
+		}
+	}
+
 	return os.Remove(path)
 }
